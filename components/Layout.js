72356e5b314f56e8b3eb006e5e65a7db4c919d25
tommaay <tommy@tomster.dev>
2022-05-25 17:49:55 -0700

    Project setup

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Layout({ title, keywords, description, children, className = "" }) {
+  return (
+    <div className="font-poppins bg-darkblue min-h-screen">
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+
+      <div className={`text-white overflow-x-hidden min-h-screen ${className}`}>{children}</div>
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "Decentrament | Decentralized Autonomous Venture Capital",
+  description:
+    "Our vision is to decentralize the world. We invest early and we help teams to build the next cutting-edge technologies in the Web3 space.",
+  keywords: "blockchain, dao, investing, decentrament, web3, vc, venture capital",
+};
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        darkblue: "rgb(40, 50, 70, 0.85)",
+        "brand-1": "#9d46eb",
+        "brand-2": "#7560da",
+        "brand-3": "#00a6ff",
+        "brand-4": "#527ace",
+      },
+    },
+  },
+  plugins: [],
+  important: true,
+};
