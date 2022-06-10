92402a2ed38764aa120a31ec58e177563a63f438
tommaay <tommy@tomster.dev>
2022-06-10 12:46:49 -0700

    Updated wrong network message

--- components/WrongNetwork.js
+++ components/WrongNetwork.js
@@ -2,10 +2,10 @@ import Layout from "./Layout";
 
 export default function WrongNetwork() {
   return (
-    <Layout>
-      <h1>Decentrament Web App</h1>
+    <Layout className="py-4 text-center">
+      <h1 className="text-3xl text-brand-3 font-bold mt-8 mb-8">Decentrament DAO</h1>
 
-      <h2 className="text-red-500">Please change your network to Polygon Mumbai</h2>
+      <h2 className="text-xl text-red-500">Please change your network to Polygon Mumbai</h2>
     </Layout>
   );
 }
